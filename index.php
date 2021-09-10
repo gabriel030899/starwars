cebcaf991feed5cbd2cd29f612cd1ce8c86fe915
Silva <Gabriel.Silva14@br.nestle.com>
2021-09-10 13:33:33 -0300

    novas paginas html e banco de dados incluido

---
+++ index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Mundo CTA Digital</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+
+
+    <body id="page-top">
+
+
+
+        <!-- Barra de Navegação  -->
+
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">MUNDO CTA</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Seja Bem Vindo ao Mundo CTA da Nestlé de São José do Rio Pardo</h2>
+                      <a href="#about">  <button id="btnSpeak" class="btn btn-primary" > Vamos lá </button> </a> 
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Esse é o novo CTA </h2>
+                        <p class="text-white-50">
+                           Aqui você vai aprender um pouco sobre todos os pilares, treinamentos, cursos e ferramentas, aproveite e acumule pontos nessa sua jornada. 
+                        </p>
+                    </div>
+                </div>
+
+            <style>
+            
+            #pilar1:hover {
+                
+                
+                background: url("assets/img/pilares_hover.png") no-repeat;
+                background-position:center;
+                background-size:contain;
+
+
+                       }
+            
+            #pilar1 {
+                
+                
+            background: url("assets/img/pilares.png") no-repeat;
+            background-position:center;
+            background-size:contain;
+
+
+
+            }
+            
+            
+            
+              #pilar2 {
+                
+                
+                background: url("assets/img/tecno.png") no-repeat;
+                                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+                        #pilar2:hover {
+                
+                
+                background: url("assets/img/tecno_hover.png") no-repeat;
+                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+            
+                        
+              #pilar3 {
+                
+                
+                background: url("assets/img/matriz.png") no-repeat;
+                                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+                        #pilar3:hover {
+                
+                
+                background: url("assets/img/matriz_hover.png") no-repeat;
+                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+                                    
+              #pilar4 {
+                
+                
+                background: url("assets/img/central.png") no-repeat;
+                                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+                        #pilar4:hover {
+                
+                
+                background: url("assets/img/central_hover.png") no-repeat;
+                                                background-position:center;
+                                background-size:contain;
+
+
+            }
+            
+            
+            
+            
+            .pilaresformat{
+                display: flex;
+                justify-content:center;
+                align-items:center;
+                width: 200px;
+                height: 700px;
+                padding:10px;
+
+            }
+            
+            
+            #mae-pilares {
+                
+                display:flex;
+                width:100%;
+                justify-content:center;
+                align-items:center;
+                
+            }
+            #carouselExampleControls{
+                margin-top:30px;
+            }
+            
+            
+            </style>
+
+
+                <div id="mae-pilares">
+
+                <a href="direciona.php?codigo=1"> <div id="pilar1" class="pilaresformat" >  </div> </a> 
+               <a href="direciona.php?codigo=2" target="_blank "> <div id="pilar2" class="pilaresformat">  </div> </a>
+                <a href="direciona.php?codigo=3" target="_blank"> <div id="pilar3" class="pilaresformat">  </div> </a>
+               <a href="direciona.php?codigo=4" target="_blank"> <div id="pilar4" class="pilaresformat">  </div> </a>
+                
+                
+                
+                 </div>
+                
+                
+            </div>
+        </section>
+        <!-- Projects-->
+        
+        
+        
+        
+        
+        
+        <section class="projects-section bg-light" id="projects">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>CTA Informa</h4>
+                            <p class="text-black-50 mb-0">Principais Destaques</p>
+                        </div>
+                    </div>
+                    
+                    
+                    
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div style="" class="carousel-item active">
+      <img style="width:100%; height:400px;" src="assets/img/47048.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img style="width:100%; height:400px;" src="assets/img/47389.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img style="width:100%; height:400px;" src="assets/img/48105.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img style="width:100%; height:400px;" src="assets/img/51419.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                    
+                    
+                    
+                </div>
+                <!-- Project One Row-->
+               
+                <!-- Project Two Row-->
+
+            </div>
+        </section>
+        <!-- Signup-->
+       
+        <!-- Contact-->
+
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy;  2021</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        
+        
+          <script src="tts.js"></script>
+    </body>
+</html>
---
+++ tts.js
@@ -0,0 +1,32 @@
+var btnSpeak = document.querySelector('#btnSpeak');
+
+speaks = [
+  {
+    "name": "Clarinha",
+    "lang":"pt-BR"
+  }
+]
+
+btnSpeak.addEventListener('click', () => {
+
+  const msg = new SpeechSynthesisUtterance();
+  msg.volume = 1; //define o volume do áudio (de 0 a 1)
+  msg.rate = 1; // define a velocidade do áudio (0.1 a 1)
+  msg.pitch = 1.5; // define o tom em que o áudio é falado ( de 0 a 2)
+  msg.text = ".. Esse é o novo Centro de Treinamento da Nestlé  de São José do Rio Pardo..  Aqui você vai aprender um pouco sobre todos os Pilares. Treinamentos. Cursos e ferramentas.. Aproveite e acumule pontos nessa sua jornada.. ";
+  
+  
+  const voice = speaks[0];
+  // console.log(`Voice: ${voice.name} and Lang: ${voice.lang}`);
+  voice.voiceURI = voice.name; //voiceURI busca o servidor da voz escolhida, no caso, pt-BR defnido em voice.name
+  msg.lang = voice.lang; // Define o idioma a ser utilizado para a fala, no caso, o que foi definido para voice.lang
+  
+  speechSynthesis.speak(msg); //executa a voz
+  
+
+});
+
+
+
+
+
